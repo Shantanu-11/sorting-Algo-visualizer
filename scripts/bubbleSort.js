21a9ec668c656d1cfcd846fa0a70f778f9f51a1e
Your Name <shantanu857@gmail.com>
2020-12-15 08:05:11 +0530

    All scripts+css file done

---
+++ scripts/bubbleSort.js
@@ -0,0 +1,25 @@
+function Bubble(){
+    c_delay=0;
+
+    for(var i=0;i<array_size;i++)
+    {
+        for(var j=0;j<array_size-1-i;j++)
+        {
+            div_update(divs[j],div_sizes[j],"#bf0603"); //yellow color
+            if(div_sizes[j]>div_sizes[j+1])
+            {
+                 div_update(divs[j],div_sizes[j],"#bf0603");//red color
+                div_update(divs[j+1],div_sizes[j+1],"#bf0603");///red
+                var temp=div_sizes[j];
+                div_sizes[j]=div_sizes[j+1];
+                div_sizes[j+1]=temp;
+                div_update(divs[j],div_sizes[j],"#03045e");//blue
+                div_update(divs[j+1],div_sizes[j+1],"#03045e");//blue
+            }
+            div_update(divs[j],div_sizes[j], "#ff6542");//Color update
+        }
+         div_update(divs[j],div_sizes[j], "#0f4c5c");//Color green update
+    }
+    div_update(divs[0],div_sizes[0], "#0f4c5c"); //color green update
+    enable_buttons();
+}
